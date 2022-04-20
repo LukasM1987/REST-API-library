07d9c263af13962e0daa77ebaf26fe67039a6fd4
Lukasz Marchel <marchel.lukas@gmail.com>
2022-04-20 11:52:45 +0200

    KOD-21.7 fixed KodillaLibraryApplicationTest class

--- src/test/java/com/crud/kodillalibrary/KodillaLibraryApplicationTests.java
+++ src/test/java/com/crud/kodillalibrary/KodillaLibraryApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KodillaLibraryApplicationTests {
+public class KodillaLibraryApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
